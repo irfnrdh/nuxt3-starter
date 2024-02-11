dcca029671c6b22da6bcb422bdffb11837cd211c
irfnrdh <kang.fand@gmail.com>
2024-02-11 15:16:39 +0700

    update pg

--- server/utils/db.instance.ts
+++ server/utils/db.instance.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-
+import pg from "pg";
 
 // SQLITE
 // export const sequelize = new Sequelize({
@@ -16,8 +16,13 @@ export const sequelize = new Sequelize({
   host: "ep-lingering-smoke-a1v62b72-pooler.ap-southeast-1.aws.neon.tech", // or your PostgreSQL host
   port: 5432, // or your PostgreSQL port
   username: "default",
+  dialectModule: pg, 
   password: "TCOyQm6tM9RV",
   database: "verceldb",
+  define: {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci',
+  },
   logging: false,
 });
 
